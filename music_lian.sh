7a5108fed99d9c2c2b39998b2633f0248b490ff5
CDWEN0526 <58065939+CDWEN0526@users.noreply.github.com>
2020-01-03 11:03:51 +0800

    Add files via upload

--- music_lian.sh
+++ music_lian.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 while true;do
-./mp3.py  >> ./music.txt
+./mp3.py  >> ./music_two.txt
+mv ./music_two.txt ./music.txt
  sleep 240
- rm -rf ./music.txt
-./mp3_1.py >> ./music.txt
+./mp3_1.py >> ./music_two.txt
+mv ./music_two.txt ./music.txt
  sleep 240
- rm -rf ./music.txt
-./mp3_2.py >> ./music.txt
+./mp3_2.py >> ./music_two.txt
+mv ./music_two.txt ./music.txt
  sleep 240
- rm -rf ./music.txt
-./mp3_3.py >> ./music.txt
+./mp3_3.py >> ./music_two.txt
+mv ./music_two.txt ./music.txt
  sleep 240
- rm -rf ./music.txt
-./mp3_4.py >> ./music.txt
+./mp3_4.py >> ./music_two.txt
+mv ./music_two.txt ./music.txt
  sleep 240
- rm -rf ./music.txt
-./mp3_5.py >> ./music.txt
+./mp3_5.py >> ./music_two.txt
+mv ./music_two.txt ./music.txt
  sleep 240
- rm -rf ./music.txt
-./mp3_12.py >> ./music.txt
+./mp3_12.py >> ./music_two.txt
+mv ./music_two.txt ./music.txt
  sleep 240
- rm -rf ./music.txt
-./mp3_14.py >> ./music.txt
+./mp3_14.py >> ./music_two.txt
+mv ./music_two.txt ./music.txt
  sleep 240
- rm -rf ./music.txt
 done

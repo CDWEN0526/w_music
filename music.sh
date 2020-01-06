70aaed8805aef238a3d66b461aa770cdcd0c2728
CDWEN0526 <58065939+CDWEN0526@users.noreply.github.com>
2020-01-06 11:45:08 +0800

    Add files via upload

--- music.sh
+++ music.sh
@@ -4,13 +4,14 @@ lian=`ps axu | grep  music_lian | grep -v grep | wc -l`
 if [ ${lian} == 0 ];then
 echo "正在获取网络播放信息，请稍等..."
 ./mp3.py  >> ./music.txt &
+nohup ./music_lian.sh &
 sleep 5
 clear
 fi
 
 while true;do 
 echo -e "\n==树莓派网络音乐程序=="
-echo -e "  1、切换歌曲\n  2、调整音量\n  3、播放音乐\n  4、终止播放\n  5、后台运行\n"
+echo -e "  1、切换歌曲\n  2、调整音量\n  3、播放音乐\n  4、终止播放\n  5、后台运行\n  6、检查更新\n"
 read -p "请输入您的操作：" number
 echo -e  "\n=======开始执行操作=======\n"
 case $number in 
@@ -39,6 +40,11 @@ exit 0
 ;;
 "5")
 echo "后台运行中..."
+exit 0
+;;
+"6")
+echo "检查程序是否有更新..."
+git pull
 exit 0
 ;;
 *)

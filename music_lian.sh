1dc4a9f9593a5992b3b9b2903b2b03593917923b
CDWEN0526 <58065939+CDWEN0526@users.noreply.github.com>
2020-01-07 12:27:45 +0800

    Add files via upload

--- music_lian.sh
+++ music_lian.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 while true;do
- sleep 300
+ sleep 240
 ./mp3_1.py >> ./music_two.txt
 mv ./music_two.txt ./music.txt
- sleep 300
+ sleep 240
 ./mp3_2.py >> ./music_two.txt
 mv ./music_two.txt ./music.txt
- sleep 300
+ sleep 240
 ./mp3_3.py >> ./music_two.txt
 mv ./music_two.txt ./music.txt
- sleep 300
+ sleep 240
 ./mp3_4.py >> ./music_two.txt
 mv ./music_two.txt ./music.txt
- sleep 300
+ sleep 240
 ./mp3_8.py >> ./music_two.txt
 mv ./music_two.txt ./music.txt
- sleep 300
+ sleep 240
 done
